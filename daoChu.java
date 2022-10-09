64c2d49044e04bfee12a4bf026e11ab1aeb9166d
nhanngo2720 <60211514+nhanngo2720@users.noreply.github.com>
2022-10-09 13:47:49 +0900

    Save file dao String

---
+++ daoChu.java
@@ -0,0 +1,11 @@
+public class daoChu
+{
+	public static void main(String[] args) {
+		String trong = "", a = "acbdef" ;
+	for (int i=a.length () -1 ;i>=0;i-- ){
+	trong += a.charAt(i);
+	}
+	System.out.println(trong);
+
+	}
+}
